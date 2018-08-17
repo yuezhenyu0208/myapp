5c1b6d7b36ca963c7d45cf50b418bf8f48bc66c3
yuezhenyu <zhenyuyue@qq.com>
2018-08-17 14:46:56 +0800

    初始导入

---
+++ src/main/java/com/yue/myspp/MysppApplication.java
@@ -0,0 +1,12 @@
+package com.yue.myspp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysppApplication.class, args);
+	}
+}
